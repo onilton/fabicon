2084733e4ebf97fb296aec6d2afb8d99cbd946e4
Onilton Maciel <oniltonmaciel@gmail.com>
2013-04-12 18:18:42 -0400

    Added business to facebook reserved word list

--- fabicon.py
+++ fabicon.py
@@ -54,7 +54,7 @@ def getTwitterRealName(twitterUsername):
     return result
 
 
-facebookUsernameExcludeList = ['media', 'permalink.php', 'YOUR_USERNAME', 'photo.php', 'groups', 'sharer.php', 'notes', 'badges']
+facebookUsernameExcludeList = ['media', 'permalink.php', 'YOUR_USERNAME', 'photo.php', 'groups', 'sharer.php', 'notes', 'badges', 'business']
 
 
 def facebookUsername(facebookPageUrl):
